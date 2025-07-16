9f33fb97845f5896bb10e8ba35858430e24dbc0b
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-07-16 16:24:51 -0300

    Realizado repositorio base em orm e DbContext

---
+++ Duobingo.InfraestruturaEmOrm/Compartilhado/RepositorioBaseEmOrm.cs
@@ -0,0 +1,60 @@
+﻿
+
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace eAgenda.Infraestrutura.Orm.Compartilhado
+{
+    public class RepositorioBaseEmOrm<T> where T : EntidadeBase<T>
+    {
+        private readonly DbSet<T> registros;
+
+        public RepositorioBaseEmOrm(eAgendaDbContext contexto)
+        {
+            this.registros = contexto.Set<T>();
+        }
+        public void CadastrarRegistro(T novoRegistro)
+        {
+            registros.Add(novoRegistro);
+        }
+
+        public bool EditarRegistro(Guid idRegistro, T registroEditado)
+        {
+            var registroSelecionado = SelecionarRegistroPorId(idRegistro);
+
+            if (registroSelecionado is null)
+                return false;
+
+            else
+            {
+                registroSelecionado.AtualizarRegistro(registroEditado);
+                return true;
+            }
+
+        }
+        public bool ExcluirRegistro(Guid idRegistro)
+        {
+            var registroSelecionado = SelecionarRegistroPorId(idRegistro);
+
+            if (registroSelecionado is null)
+                return false;
+
+
+            else
+            {
+                registros.Remove(registroSelecionado);
+                return true;
+            }
+        }
+
+        public virtual T? SelecionarRegistroPorId(Guid idRegistro)
+        {
+            return registros.FirstOrDefault(x => x.Id.Equals(idRegistro));
+        }
+
+        public virtual List<T> SelecionarRegistros()
+        {
+            return registros.ToList();
+        }
+    }
+}
---
+++ Duobingo.InfraestruturaEmOrm/Compartilhado/duobingoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using eAgenda.Dominio.ModuloContato;
+using eAgenda.Dominio.ModuloCompromisso;
+using Microsoft.EntityFrameworkCore;
+
+namespace eAgenda.Infraestrutura.Orm.Compartilhado
+{
+    public class duobingoDbContexto : DbContext
+    {
+        public duobingoDbContexto(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(duobingoDbContexto).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
