aa0f50f939b4d3d549c8d11ab852d9a0b58d2068
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-07-17 14:18:03 -0300

    Criado classe de dominio materia

---
+++ Duobingo.Dominio/ModuloMateria/Materia.cs
@@ -0,0 +1,19 @@
+﻿
+using Duobingo.Dominio.Compartilhado;
+
+namespace Duobingo.Dominio.ModuloMateria
+{
+    public class Materia : EntidadeBase<Materia>
+    {
+        public string Nome { get; set; }
+        public Disciplina Disciplina { get; set; }
+        public string Serie { get; set; }
+        public override void AtualizarRegistro(Materia registroEditado)
+        {
+            Id = Guid.NewGuid();
+            Nome = registroEditado.Nome;
+            Disciplina = registroEditado.Disciplina;
+            Serie = registroEditado.Serie;
+        }
+    }
+}
