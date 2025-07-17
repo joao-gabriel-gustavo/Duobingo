b16e15e39e3227a14f0c3d634b27cbc7da98f8f8
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-07-17 14:37:01 -0300

    Realizado a parte de action filter e injeção de dependencias

---
+++ Duobingo.WebApp/ActionFilters/LogarAcaoAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Duobingo.WebApp.ActionFilters
+{
+    public class LogarAcaoAttribute : ActionFilterAttribute
+    {
+        private readonly ILogger<LogarAcaoAttribute> logger;
+
+        public LogarAcaoAttribute(ILogger<LogarAcaoAttribute> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            var result = context.Result;
+
+            if (result is ViewResult viewResult && viewResult.Model is not null)
+            {
+                logger.LogInformation(
+                    "Ação de endpoint executada com sucesso! {@Modelo}", viewResult.Model);
+            }
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
