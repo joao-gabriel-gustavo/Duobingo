f66958dd4b4f69deaf80ddba7102853fcab9c5e2
Gustavo Duara <gugaduara@gmail.com>
2025-07-17 14:39:47 -0300

    criado moduloDisciplina

---
+++ Duobingo.Dominio/ModuloDisciplina/Disciplina.cs
@@ -0,0 +1,23 @@
+﻿using Duobingo.Dominio.Compartilhado;
+
+namespace Duobingo.Dominio.ModuloDisciplina;
+
+public class Disciplina : EntidadeBase<Disciplina>
+{
+    public string Nome { get; set; }
+
+    public Disciplina()
+    {
+    }
+
+    public Disciplina(string nome)
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+    }
+
+    public override void AtualizarRegistro(Disciplina registroEditado)
+    {
+        Nome = registroEditado.Nome;
+    }
+}
---
+++ Duobingo.Dominio/ModuloDisciplina/IRepositorioDisciplina.cs
@@ -0,0 +1,9 @@
+﻿using Duobingo.Dominio.Compartilhado;
+using Duobingo.Dominio.ModuloDisciplina;
+
+namespace DuoBingo.Dominio.ModuloDisciplina
+{
+    public interface IRepositorioDisciplina : IRepositorio<Disciplina>
+    {
+    }
+}
