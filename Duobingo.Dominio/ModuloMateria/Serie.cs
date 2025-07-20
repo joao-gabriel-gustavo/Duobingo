0312a84bca0b8d3784ed6dc105366988326a964e
Gustavo Duara <gugaduara@gmail.com>
2025-07-20 16:30:26 -0300

    Adicionado Enum Serie ModuloDisciplina

---
+++ Duobingo.Dominio/ModuloMateria/Serie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duobingo.Dominio.ModuloMateria
+{
+    public enum Serie
+    {
+        Primeiro,
+        Segundo,
+        Terceiro,
+        Quarto,
+        Quinto
+    }
+}
