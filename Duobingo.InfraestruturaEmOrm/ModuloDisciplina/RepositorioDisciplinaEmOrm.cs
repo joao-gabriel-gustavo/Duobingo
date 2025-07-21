bf8b0245e668653fb75710815e0bfbfaa5f45bd9
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-07-20 22:22:18 -0300

    Adicionado repositorio disciplina em orm

---
+++ Duobingo.InfraestruturaEmOrm/ModuloDisciplina/RepositorioDisciplinaEmOrm.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Duobingo.Dominio.ModuloDisciplina;
+using Duobingo.Infraestrutura.Orm.Compartilhado;
+using DuoBingo.Dominio.ModuloDisciplina;
+
+namespace Duobingo.InfraestruturaEmOrm.ModuloDisciplina
+{
+    public class RepositorioDisciplinaEmOrm : RepositorioBaseEmOrm<Disciplina>, IRepositorioDisciplina
+    {
+        public RepositorioDisciplinaEmOrm(duobingoDbContext contexto) : base(contexto)
+        {
+        }
+    }
+}
